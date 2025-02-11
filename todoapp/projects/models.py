51f7ff9050e0c6151a1150b540b6cddc497b9e07
hamid-JTG <hamid.husain@joshtechnologygroup.com>
2025-02-11 10:28:22 +0530

    exercise4

--- todoapp/projects/models.py
+++ todoapp/projects/models.py
@@ -1,8 +1,24 @@
 
 from django.db import models
-
+from django.conf import settings
+from users.models import CustomUser
 
 class Project(models.Model):
+    members = models.ManyToManyField(CustomUser, through='ProjectMember', related_name='projects')
+    name = models.CharField(max_length=100)
+    max_members=models.PositiveIntegerField
+    
+    CHOICES = [
+        (0,"To be started"),
+        (1,"In Progress"),
+        (2,"Completed")
+    ]
+
+    status = models.IntegerField(choices=CHOICES, default=0)
+
+    def __str__(self):
+        return f"{self.name}"
+
     """
         Needed fields
         - members (m2m field to CustomUser; create through table and enforce unique constraint for user and project)
@@ -15,6 +31,17 @@ class Project(models.Model):
 
 
 class ProjectMember(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE) 
+    member = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["project", "member"], name="unique_project_member")
+        ]
+
+    def __str__(self):
+        return f"{self.project} {self.member.email}"
+    
     """
     Needed fields
     - project (fk to Project model)
--- todoapp/users/models.py
+++ todoapp/users/models.py
@@ -4,16 +4,40 @@ from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import BaseUserManager
 from django.contrib.auth.models import PermissionsMixin
 from django.db import models
+from django.utils.timezone import now
 
 
 class UserManager(BaseUserManager):
+
     """
     Add manager methods here to create user and super user
     """
 
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The email is needed")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)  
+        user.save(using=self._db)  
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(email, password, **extra_fields)
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+    email = models.EmailField(unique=True)
+    date_joined = models.DateTimeField(default=now)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    USERNAME_FIELD = 'email'  
+    REQUIRED_FIELDS = ['first_name', 'last_name']  
+
     """
     Needed fields
     - password (already inherited from AbstractBaseUser; encrypt password before saving to database)
