1c321b19592d6155ec5ab4dbfd16893fe76ca84f
hamid-JTG <hamid.husain@joshtechnologygroup.com>
2025-02-19 19:41:01 +0530

    Revert "serializers and utils"
    This reverts commit 780f1a075d5a4dd98d33616ebb9b77ab818272ae.

---
+++ todoapp/todos/models.py
@@ -0,0 +1,26 @@
+from django.conf import settings
+from django.db import models
+from django.utils.timezone import now
+
+
+class Todo(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             related_name='todos', on_delete=models.CASCADE)
+    name = models.CharField(max_length=1000)
+    done = models.BooleanField(default=False)
+    date_created = models.DateTimeField(default=now)
+    date_completed = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+    """
+        Needed fields
+        - user (fk to User Model - Use AUTH_USER_MODEL from django.conf.settings)
+        - name (max_length=1000)
+        - done (boolean with default been false)
+        - date_created (with default of creation time)
+        - date_completed (set it when done is marked true)
+
+        Add string representation for this model with todos name.
+    """
