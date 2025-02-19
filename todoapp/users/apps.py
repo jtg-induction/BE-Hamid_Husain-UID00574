780f1a075d5a4dd98d33616ebb9b77ab818272ae
hamid-JTG <hamid.husain@joshtechnologygroup.com>
2025-02-19 19:39:17 +0530

    serializers and utils

--- todoapp/users/apps.py
+++
@@ -1,8 +0,0 @@
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class UsersConfig(AppConfig):
-    name = 'users'
-    verbose_name = 'Basic user system for the TODOs application'
